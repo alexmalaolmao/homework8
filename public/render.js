079ab104111024919abb5afdf5eeb96a8257f8a8
alexmalao <alexmalao98@gmail.com>
2020-11-17 13:13:02 -0500

    animating the cube

--- public/render.js
+++ public/render.js
@@ -17,6 +17,12 @@ const drawScene = (gl, parameters, buffers, deltaTime) => {
         squareRotation,
         [0, 0, 1]);
 
+    glMatrix.mat4.rotate(
+        modelViewMatrix,
+        modelViewMatrix,
+        squareRotation * .7,
+        [0, 1, 0]);
+
     squareRotation += deltaTime;
     
     configurePositionBufferRead(gl, buffers, parameters);
@@ -24,9 +30,12 @@ const drawScene = (gl, parameters, buffers, deltaTime) => {
     gl.useProgram(parameters.program);
     setUniforms(gl, parameters,
         projectionMatrix, modelViewMatrix);
-    gl.drawArrays(gl.TRIANGLE_STRIP,
-        0, // offset
-        4); // vertexCount
+    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, buffers.indices);
+    gl.drawElements(
+        gl.TRIANGLES,
+        36,
+        gl.UNSIGNED_SHORT,
+        0);
 }
 
 const clearScene = (gl) => {
@@ -57,7 +66,7 @@ const configurePositionBufferRead =
     gl.bindBuffer(gl.ARRAY_BUFFER, buffers.position);
     gl.vertexAttribPointer(
           parameters.attribLocations.vertexPosition,
-          2,
+          3,
           gl.FLOAT,
           false,
           0,
