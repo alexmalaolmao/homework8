f41b8b78973d703b384c848561a439a7810216bf
alexmalao <alexmalao98@gmail.com>
2020-11-17 13:06:56 -0500

    add colors per vertex

--- public/buffers.js
+++ public/buffers.js
@@ -1,10 +1,28 @@
 const initializeBuffers = (gl) => {
 
     const positionBuffer = initializePositionBuffer(gl);
+    const colorBuffer = initializeColorBuffer(gl);
 
     return {
         position: positionBuffer,
+        color: colorBuffer,
     };
+}
+
+const initializeColorBuffer = (gl) => {
+    const colors = [
+       1.0,  1.0,  1.0,  1.0,    // vertex 0 is white
+       1.0,  0.0,  0.0,  1.0,    // vertex 1 is red
+       0.0,  1.0,  0.0,  1.0,    // vertex 2 is green
+       0.0,  0.0,  1.0,  1.0,    // vertex 3 is blue
+    ];
+
+    const colorBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, colorBuffer);
+    gl.bufferData(gl.ARRAY_BUFFER,
+        new Float32Array(colors), gl.STATIC_DRAW);
+
+    return colorBuffer
 }
 
 const initializePositionBuffer = (gl) => {
